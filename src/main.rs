edbb9e50d65aabe2eb9a22a2b9a5c6eb47f469c9
Garageman <bbrantingham@gmail.com>
2023-01-03 11:05:21 -0800

    Writing Out

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 extern crate rand;
 use rand::Rng;
 use std::collections::{HashMap};
+use std::fs::File;
+use std::io::prelude::*;
 
 #[derive(Copy, Clone)]
 struct Agent
@@ -16,20 +18,28 @@ impl Agent
         let options: [[i32;2];4] = [[0,1],[0,-1],[-1,0],[1,0]];
         let delta: [i32;2] = options[rand::thread_rng().gen_range(0..4)];
         //todo: there is an issue here, probably in the adding code, where no new positions are chosen
-        //println!("Agent {} started at {},{}",self.id,self.position[0],self.position[1]);
 
         self.position = [self.position[0] + delta[0], self.position[1] + delta[1]];
 
         let count = grid.entry((self.position[0],self.position[1])).or_insert(0);
-        //println!("Agent {} moved to {},{}",self.id,self.position[0],self.position[1]);
         *count += 1;
     }
 }
 
+fn write_out(item: &HashMap<(i32,i32),i32>) ->std::io::Result<()>
+{
+    let mut file = File::create("Output.txt")?;
+    for (key,val) in item.iter()
+    {
+        file.write_all(format!("({},{}) was visited {} times \n", key.0,key.1, val).as_bytes())?;
+    }
+    return Ok(());
+}
+
 fn main() {
     println!("Hello, world!");
-    const NUMBER_AGENTS:usize = 50;
-    const STEPS:usize = 50;
+    const NUMBER_AGENTS:usize = 100;
+    const STEPS:usize = 100;
     let mut agents:[Agent;NUMBER_AGENTS] = [Agent{position:[0,0], id: 0};NUMBER_AGENTS];
     let mut grid  = HashMap::new();
     grid.insert((0,0),NUMBER_AGENTS as i32);
@@ -48,10 +58,6 @@ fn main() {
         println!("Step {} completed", i);
     }
 
-    for (key,val) in grid.iter()
-    {
-        println!("({},{}) was visited {} times", key.0,key.1, val);
-    }
-
+    write_out(&grid).ok();
 
 }
